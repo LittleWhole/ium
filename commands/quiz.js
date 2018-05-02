06299a3ce67eb82f101caf11e5a3081ac13b3b5b
tetraaaaa <tetragamingtg@gmail.com>
2018-05-02 07:59:14 -0700

    quiz [dev]

---
+++ commands/quiz.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js');
+const quiz = ;
+const options = {
+  max: 1,
+  time: 30050,
+  errors: ["time"],
+};
+
+module.exports.run = async (bot, message, args) => {
+  
+  const item = quiz[Math.floor(Math.random() * quiz.length)];
+  await message.channel.send(item.q);
+  try {
+    const collected = await message.channel.awaitMessages(answer => item.a.includes(answer.content.toLowerCase()), options);
+    const winnerMessage = collected.first();
+    return message.channel.send({embed: new Discord.RichEmbed()
+                                 .setAuthor(`Winner: ${winnerMessage.author.tag}`, winnerMessage.author.displayAvatarURL)
+                                 .setTitle(`Correct Answer: \`${winnerMessage.content}\``)
+                                 .setFooter(`Question: ${item.q}`)
+                                 .setColor(message.guild.me.displayHexColor)
+                                })
+  } catch (_) {
+    return message.channel.send({embed: new Discord.RichEmbed()
+                                 .setAuthor('No one got the answer in time!')
+                                 .setTitle(`Correct Answer(s): \`${item.a}\``)
+                                 .setFooter(`Question: ${item.q}`)
+                                })
+  }
+}
+module.exports.help = {
+name: "quiz"
+}
