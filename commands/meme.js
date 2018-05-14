ba87e0f9177a56b5e3ebad1c52ea88579327ddf1
tetraaaaa <tetragamingtg@gmail.com>
2018-05-13 19:58:13 -0700

    meme emoji

--- commands/meme.js
+++ commands/meme.js
@@ -3,10 +3,11 @@ const Discord = require("discord.js");
 const meme = require('memejs');
 
 exports.run = async (bot, message, args, level) => {
+	let searchMessage = await < Message > .reply('<a:typing:393848431413559296> Searching for memes in the database... ');
   meme(function(data) {
   const embed = new Discord.RichEmbed()
   .setTitle(data.title[0])
   .setColor("RANDOM")
   .setImage(data.url[0])
-  message.channel.send({embed});
+  searchMessage.edit({embed});
 })};
