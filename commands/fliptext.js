1131f60ee3c18f9cf7f5297deff9d5a424d85914
tetraaaaa <tetragamingtg@gmail.com>
2018-05-13 19:33:03 -0700

    fliptext

---
+++ commands/fliptext.js
@@ -0,0 +1,15 @@
+const mapping = '¡"#$%⅋,)(*+\'-˙/0ƖᄅƐㄣϛ9ㄥ86:;<=>¿@∀qƆpƎℲפHIſʞ˥WNOԀQɹS┴∩ΛMX⅄Z[/]^_`ɐqɔpǝɟƃɥᴉɾʞlɯuodbɹsʇnʌʍxʎz{|}~';
+const OFFSET = '!'.charCodeAt(0);
+
+exports.run = async (client, message, args, level) => {
+  if (args.length < 1) {
+    message.channel.send('**You must provide text to flip.** `ium fliptext eeee`');
+}
+
+message.channel.send(
+    args.join(' ').split('')
+        .map(c => c.charCodeAt(0) - OFFSET)
+        .map(c => mapping[c] || ' ')
+        .reverse().join('')
+);
+};
