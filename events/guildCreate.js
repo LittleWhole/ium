d1e4600c83f2e96b04b80cb1e0f60037606c313e
tetraaaaa <tetragamingtg@gmail.com>
2018-05-13 20:27:20 -0700

    Better Stats

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 let inline = true;
 
-module.exports = async (bot, guild, member) => {
+module.exports = (bot, guild, member) => {
         const verlvl = {
             0: "None",
             1: "Low",
@@ -9,10 +9,6 @@ module.exports = async (bot, guild, member) => {
             3: "(╯°□°）╯︵ ┻━┻",
             4: "(ノಠ益ಠ)ノ彡┻━┻"
         }
-
-    const invite = await guild.channels.first().createInvite({
-        maxAge: 0
-    });
 
     let sicon = guild.iconURL;
     let guildEmbed = new Discord.RichEmbed()
@@ -22,13 +18,14 @@ module.exports = async (bot, guild, member) => {
     .setDescription("ium has been **added** to a server :)")
     .addField("Guild", `${guild}`, inline)
     .addField("Users", `${guild.memberCount}`, inline)
-    .addField("Owner", guild.owner, inline)
     .addField("Region", guild.region, inline)
     .addField("Roles", guild.roles.size, inline)
     .addField("Channels", guild.channels.size, inline)
+    .addField("Creation", guild.createdAt)
+    .addField("Owner", `${guild.owner} | \`${guild.ownerID}\``)
     .setFooter(`ID - ${guild.id}`)
     .setTimestamp();
 
     bot.channels.filter(c => c.id === '434521909745549333').forEach(channel => channel.send(guildEmbed));
-    bot.channels.filter(c => c.id === '434521909745549333').forEach(channel => channel.send(`https://discord.gg/${invite.code}`));
+
   };
--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 let inline = true;
 
-module.exports = async (bot, guild, member) => {
+module.exports = (bot, guild, member) => {
         const verlvl = {
             0: "None",
             1: "Low",
@@ -10,24 +10,21 @@ module.exports = async (bot, guild, member) => {
             4: "(ノಠ益ಠ)ノ彡┻━┻"
         }
 
-    const invite = await guild.channels.first().createInvite({
-        maxAge: 0
-    });
-
     let sicon = guild.iconURL;
     let guildEmbed = new Discord.RichEmbed()
+    .setAuthor("ium", "https://ium-bot.github.io/ium.jpg")
     .setColor('#34e7e4')
     .setThumbnail(sicon)
     .setDescription("ium has been **removed** from a server :(")
     .addField("Guild", `${guild}`, inline)
     .addField("Users", `${guild.memberCount}`, inline)
-    .addField("Owner", guild.owner, inline)
     .addField("Region", guild.region, inline)
     .addField("Roles", guild.roles.size, inline)
     .addField("Channels", guild.channels.size, inline)
+    .addField("Creation", guild.createdAt)
+    .addField("Owner", `${guild.owner} | \`${guild.ownerID}\``)
     .setFooter(`ID - ${guild.id}`)
     .setTimestamp();
 
     bot.channels.filter(c => c.id === '434521909745549333').forEach(channel => channel.send(guildEmbed));
-    bot.channels.filter(c => c.id === '434521909745549333').forEach(channel => channel.send(`https://discord.gg/${invite.code}`));
   };
