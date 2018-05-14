4cadc0c72bb123631d29b8b7ff8016eedbb4ec99
tetraaaaa <tetragamingtg@gmail.com>
2018-05-13 19:58:54 -0700

    fixed

--- commands/meme.js
+++ commands/meme.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 const meme = require('memejs');
 
 exports.run = async (bot, message, args, level) => {
-	let searchMessage = await < Message > .reply('<a:typing:393848431413559296> Searching for memes in the database... ');
+	let searchMessage = await message.channel.send('<a:typing:393848431413559296> Searching for memes in the database... ');
   meme(function(data) {
   const embed = new Discord.RichEmbed()
   .setTitle(data.title[0])
