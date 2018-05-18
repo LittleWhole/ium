a6634a0d473d891a6409e52fa9435cef85c7bc70
tetraaaaa <tetragamingtg@gmail.com>
2018-05-18 14:17:20 -0700

    8ball w Command Handler

--- commands/8ball.js
+++ commands/8ball.js
@@ -2,23 +2,24 @@ const Discord = require("discord.js")
 
 module.exports = {
 	name: '8ball',
-	description: 'Display info about this server.',
-
-   if(!args[1]) return message.channel.send("**Type a question with two or more words!** `ium 8ball Am I a furry`");
-   let replies = ["Yes", "No", "I don't know", "Ask again later!", "Nope", "I am not sure!", "Pls No", "You tell me"];
-
-   let result = Math.floor((Math.random() * replies.length));
-   let question = args.join(" ");
-
-   let ballembed = new Discord.RichEmbed()
-   .setAuthor(message.author.username)
-   .setColor("#000000") 
-   .addField("Question", question)
-   .addField("Answer", replies[result]);
-
-   message.channel.send(ballembed)
-}
-
-module.exports.help = {
-    name: "8ball"
-}
+    description: 'Display info about this server.',
+    aliases: ['eightball'],
+    usage: '[Am I fat?]',
+    args: true,
+    execute(message, args) {
+        //if(!args[1]) return message.channel.send("**Type a question with two or more words!** `ium 8ball Am I a furry`");
+        let replies = ["Yes", "No", "I don't know", "Ask again later!", "Nope", "I am not sure!", "Pls No", "You tell me"];
+     
+        let result = Math.floor((Math.random() * replies.length));
+        let question = args.join(" ");
+     
+        let ballembed = new Discord.RichEmbed()
+        .setAuthor(message.author.username)
+        .setColor("#000000") 
+        .addField("Question", question)
+        .addField("Answer", replies[result]);
+     
+        message.channel.send(ballembed)
+    },
+};
+    
