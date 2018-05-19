bfac4ec485b2eabcef6c65a3b233bd43794cf238
tetraaaaa <tetragamingtg@gmail.com>
2018-05-18 18:27:35 -0700

    Autouser, and Avatar Command Handler

--- commands/autouser.js
+++ commands/autouser.js
@@ -2,23 +2,29 @@ const Discord = require("discord.js");
 const sm = require("string-similarity");
 const send = require("quick.hook");
 
-module.exports.run = async(bot, message, args) => {
-
-    if(!args[0]) return message.reply("You need to type some text!");
-    let members = [];
-    let indexes = [];
-    message.guild.members.forEach(function(member) {
-        members.push(member.user.username);
-        indexes.push(member.id);
-    })
-    let match = sm.findBestMatch(args.join(' '), members);
-    let username = match.bestMatch.target;
-    let member = message.guild.members.get(indexes[members.indexOf(username)]);
+module.exports = {
+	name: 'autouser',
+    description: 'Finds the closest user that resembles that name.',
+    aliases: ['automember', 'completeuser', 'autoname'],
+    usage: '<user>',
+    args: true,
+    execute(message, args) {
+        if(!args[0]) return message.reply("You need to type some text!");
+        let members = [];
+        let indexes = [];
+        message.guild.members.forEach(function(member) {
+            members.push(member.user.username);
+            indexes.push(member.id);
+        })
+        let match = sm.findBestMatch(args.join(' '), members);
+        let username = match.bestMatch.target;
+        let member = message.guild.members.get(indexes[members.indexOf(username)]);
+        
+        const memberE = new Discord.RichEmbed()
+        .setColor('#000000')
+        .setDescription(`${member.user.tag}`);
     
-    const memberE = new Discord.RichEmbed()
-    .setColor('#000000')
-    .setDescription(`${member.user.tag}`);
-
-    message.channel.send(memberE);
+        message.channel.send(memberE);
+    },
+};
     
-}
--- commands/avatar.js
+++ commands/avatar.js
@@ -1,20 +1,23 @@
 const Discord = require("discord.js")
 
-exports.run = async (bot, message, args) => {
-    let msg = await message.channel.send("Generating avatar...");
-    let mentionedUser = message.mentions.users.first() || message.author;
-
-    let avatarEmbed = new Discord.RichEmbed()
-    .setImage(mentionedUser.displayAvatarURL)
-    .setColor(`RANDOM`)
-    .setTitle(`Avatar`)
-    .setDescription("[Avatar Link]("+mentionedUser.displayAvatarURL+")")
-    .setFooter(`Requested by ${message.author.tag}`);
-    message.channel.send(avatarEmbed)
-    msg.delete();
-}
-
-
-module.exports.help = {
-    name: "avatar"
-}
+module.exports = {
+	name: 'autouser',
+    description: 'Displays an enlarged image of a user\'s avatar, with a direct link to it.',
+    aliases: ['pfp', 'profilepicture', 'icon'],
+    usage: '<user>',
+    args: true,
+    execute(message, args) {
+        let msg = await message.channel.send("Generating avatar...");
+        let mentionedUser = message.mentions.users.first() || message.author;
+    
+        let avatarEmbed = new Discord.RichEmbed()
+        .setImage(mentionedUser.displayAvatarURL)
+        .setColor(`RANDOM`)
+        .setTitle(`Avatar`)
+        .setDescription("[Avatar Link]("+mentionedUser.displayAvatarURL+")")
+        .setFooter(`Requested by ${message.author.tag}`);
+        message.channel.send(avatarEmbed)
+        msg.delete();
+    },
+};
+    
