a8594162ddf1835bb12c75b5bb0998eff3a870b4
tetragamingtg@gmail.com <tetragamingtg@gmail.com>
2018-05-20 11:24:28 -0700

    Choose Command Handler

--- commands/choose.js
+++ commands/choose.js
@@ -1,19 +1,17 @@
 const Discord = require("discord.js")
 
-module.exports.run = async (bot, message, args) => {
-
-   if(!args[1]) return message.channel.send("**Enter two inputs for me to choose from.** `ium choose Bread Eggs`");
-
-   let replies = [`${args[0]}`, `${args[1]}`];
-   let result = Math.floor((Math.random() * replies.length));
-
-   let chooseEmbed = new Discord.RichEmbed()
-   .setColor("#000000")
-   .addField("I choose...", replies[result]);
-   message.channel.send(chooseEmbed)
-}
-
-
-module.exports.help = {
-    name: "choose"
-}
+module.exports = {
+    name: 'choose',
+    description: 'ium chooses something between the two parameters you give it.',
+    usage: 'Bread Eggs',
+    args: true,
+	execute(message) {
+        let replies = [`${args[0]}`, `${args[1]}`];
+        let result = Math.floor((Math.random() * replies.length));
+     
+        let chooseEmbed = new Discord.RichEmbed()
+        .setColor("#000000")
+        .addField("I choose...", replies[result]);
+        message.channel.send(chooseEmbed)
+	},
+};
