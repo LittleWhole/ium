5030e75f0164f71ab39cc438832544a97c3cbabd
tetragamingtg@gmail.com <tetragamingtg@gmail.com>
2018-05-20 11:38:23 -0700

    Clear command handler

--- commands/clear.js
+++ commands/clear.js
@@ -2,16 +2,29 @@ const Discord = require("discord.js");
 const config = require("../botconfig.json");
 const errors = require("../utils/errors.js")
 
-exports.run = async (bot, message, args) => {
+module.exports = {
+  name: 'clear',
+  description: 'Deletes a certain amount of messages you give it. (Cannot delete more that 100 messages at once, and messages that are 14 days or older.)',
+  aliases: ['purge', 'delete', 'prune'],
+  usage: '<value>',
+  args: true,
+	execute(bot, message, args) {
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "clear");
 
-  if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "clear");
-  if(!args[0]) return message.channel.send("**Specify an amount of messages for me to clear.** `ium clear [amount of messages to clear]` (Note: cannot clear more than 99 messages, cannot clear messages older than 2 weeks)");
+		const amount = parseInt(args[0]) + 1;
 
-  message.channel.bulkDelete(args[0]).then(() => {
-  message.delete().catch(O_o=>{});
-  message.channel.send(`Deleted **${args[0]}** messages.`).then(msg => msg.delete(5000));
+		if (isNaN(amount)) {
+			return message.reply('**That doesn\'t seem to be a valid number.**');
+		}
+		else if (amount <= 1 || amount > 100) {
+			return message.reply('**You must to provide a number between 1 and 99.**');
+		}
 
-});
-
-
-}
+		message.channel.bulkDelete(amount, true).catch(err => {
+			console.error(err);
+			message.channel.send('**There was an error trying to prune messages in this channel!** (Cannot delete more that 100 messages at once, and messages that are 14 days or older.)');
+    });
+    
+    message.channel.send(`Deleted **${args[0]}** messages.`).then(msg => msg.delete(5000));
+	},
+};
