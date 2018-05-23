a754723072d4e64de986e574b08b319331a3b5c0
tetragamingtg@gmail.com <tetragamingtg@gmail.com>
2018-05-23 07:43:13 -0700

    kick, leave, kill command handler

--- commands/leave.js
+++ commands/leave.js
@@ -1,8 +1,11 @@
 const errors = require("../utils/errors.js")
 
-module.exports.run = async (bot, message) => {
-
+module.exports = {
+    name: 'leave',
+    description: 'Makes ium leave the server it\s in.',
+	execute(bot, message, args){
     if (!['275831434772742144',].includes(message.author.id)) return errors.noTetra(message, "leave");
     message.channel.send('**Leaving** server...')
     message.guild.leave();
-  };
+	},
+};
