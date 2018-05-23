1c1f76c016d5fac1c748c53817952677c242a90f
tetragamingtg@gmail.com <tetragamingtg@gmail.com>
2018-05-23 06:54:48 -0700

    Invites Command Handler and slight change

--- commands/invite.js
+++ commands/invite.js
@@ -1,19 +1,15 @@
 const Discord = require("discord.js");
 let inline = true
 
-
-exports.run = async (bot, message, args) => {
-    let botAvatar = bot.user.displayAvatarURL;
-    let botEmbed = new Discord.RichEmbed()
-    .setColor('#000000')
-    .setDescription("Invite ium: https://ium-bot.github.io/invite")
-
-
-
-    message.channel.send(botEmbed);
-}
-
-
-module.exports.help = {
-    name: "invite"
-}
+module.exports = {
+    name: 'invite',
+    description: 'Invite link for ium.',
+	execute(bot, message, args){
+        let botAvatar = bot.user.displayAvatarURL;
+        let botEmbed = new Discord.RichEmbed()
+        .setColor('#000000')
+        .setDescription("Invite ium: https://ium-bot.github.io/invite")
+    
+        message.channel.send(botEmbed);
+	},
+};
--- commands/invites.js
+++ commands/invites.js
@@ -2,29 +2,24 @@ const Discord = require('discord.js'),
     arraySort = require('array-sort'),
     table = require('table');
 
-exports.run = async (bot, message, args, tools) => {
-
-    let invites = await message.guild.fetchInvites().catch(error => {
-        return message.channel.send('***I dont have the proper permission to access server invites!***');
-    });
-
-    invites = invites.array();
-
-    arraySort(invites, 'users', { reverse: true });
-
-    let possibleInvites = [['User', 'User']];
-    invites.forEach(function(invite) {
-        possibleInvites.push([invite.inviter.username, invite.uses]);
-    });
-
-    const embed = new Discord.RichEmbed()
-        .setColor("#000000")
-        .setTitle('Server Invites')
-        .addField('Leaderboard', `\`\`\`${table.table(possibleInvites)}\`\`\``);
-
-    message.channel.send(embed);
-}
-
-module.exports.help = {
-    name: "invites"
-  }
+module.exports = {
+    name: 'invites',
+    description: 'Shows how many members a user has invited.',
+	async execute(bot, message, args){
+        let invites = await message.guild.fetchInvites().catch(error => {
+            return message.channel.send('***I dont have the proper permission to access server invites!***');
+        });
+    
+        invites = invites.array();
+    
+        arraySort(invites, 'users', { reverse: true });
+    
+        let possibleInvites = [['User', 'User']];
+        invites.forEach(function(invite) {
+            possibleInvites.push([invite.inviter.username, invite.uses]);
+        });
+    
+    
+        message.channel.send(`**Server Invites**\nLeaderboard\n\`\`\`${table.table(possibleInvites)}\`\`\``);
+	},
+};
