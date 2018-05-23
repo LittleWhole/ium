869eb6e4de0e1ce17643b09df8a059f1fa1965b2
tetragamingtg@gmail.com <tetragamingtg@gmail.com>
2018-05-23 06:57:59 -0700

    Joke, and join command handler

--- commands/join.js
+++ commands/join.js
@@ -4,15 +4,15 @@ const tokens = require('../botconfig.json');
 
 let queue = {};
 
-exports.run = (bot, message, args) => {
-  return new Promise((resolve, reject) => {
-const voiceChannel = message.member.voiceChannel;
-if (!voiceChannel || voiceChannel.type !== 'voice') return message.reply('I couldn\'t connect to your voice channel...');
-voiceChannel.join().then(connection => resolve(connection)).catch(err => reject(err));
-});
-}
-
-
-module.exports.help = {
-    name: "join"
-}
+module.exports = {
+    name: 'join',
+    description: 'Joins the voice channel you are in.',
+    aliases: ['connect'],
+    execute(bot, message, args){
+      return new Promise((resolve, reject) => {
+        const voiceChannel = message.member.voiceChannel;
+        if (!voiceChannel || voiceChannel.type !== 'voice') return message.reply('I couldn\'t connect to your voice channel...');
+        voiceChannel.join().then(connection => resolve(connection)).catch(err => reject(err));
+      });
+    },
+};
--- commands/joke.js
+++ commands/joke.js
@@ -1,12 +1,12 @@
 const Discord = require("discord.js");
 let giveMeAJoke = require('give-me-a-joke');;
 
-module.exports.run = async (bot, message, args) => {
-    giveMeAJoke.getRandomDadJoke(function(joke){
-        message.channel.send(joke)
-    })
-}
-
-module.exports.help = {
-    name: "joke"
-}
+module.exports = {
+    name: 'joke',
+    description: 'Sends a random joke',
+	execute(bot, message, args){
+        giveMeAJoke.getRandomDadJoke(function(joke){
+            message.channel.send(joke)
+        })
+	},
+};
