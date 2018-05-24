7c4da1e9581054a71125f0ba0064a92753efe670
LittleWhole <wholelittle@gmail.com>
2018-05-23 22:05:51 -0400

    Add SQLite database with suggestion ID.
    This is so if people send pull requests, addressing a suggestion, they can say in the PR description what suggestion ID it corresponds to.

--- commands/suggest.js
+++ commands/suggest.js
@@ -1,4 +1,9 @@
 const Discord = require('discord.js');
+var fs = require('fs');
+var dbFile = './.data/sqlite.db';
+var exists = fs.existsSync(dbFile);
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database(dbFile);
 
 module.exports = {
 	name: 'suggest',
@@ -6,10 +11,21 @@ module.exports = {
     usage: '<suggestion>',
     args: true,
     execute(bot, message, args){
+	db.serialize(function(){
+  		if (!exists) {
+    			db.run('CREATE TABLE suggestions ( ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, Content varchar(2000), Author varchar(32) );');
+    			console.log('New table suggestions created!');
+  		}
+	})
+	db.run(`INSERT INTO suggestions (Content, Author) VALUES (${args.join(" ")}, ${message.author.id});`);
+	db.each(`SELECT * FROM suggestions WHERE Author=${message.author.id};`, (err, row) => {
+		const suggestionID = row.ID;	
+	});
         let suggestEmbed = new Discord.RichEmbed()
         .setAuthor("Suggestion", "https://ium-bot.github.io/ium.jpg")
         .addField("User", `${message.author.tag}`)
         .addField("Suggestion", `${args.join(" ")}`)
+	.setFooter(suggestionID)
         .setTimestamp();
         bot.channels.filter(c => c.id === '415707252486438913').forEach(channel => channel.send(suggestEmbed));
         message.channel.send(`<:check:314349398811475968> Your suggestion has been sent! Thanks!`);
