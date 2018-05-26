74d822cea2c5cf566bc8dee05fd2b6586aff6499
tetragamingtg@gmail.com <tetragamingtg@gmail.com>
2018-05-26 09:02:53 -0700

    warnlvl command handler

--- commands/warnlevel.js
+++ commands/warnlevel.js
@@ -4,16 +4,17 @@ const fs = require("fs");
 const ms = require("ms");
 let warns = require("../warnings.json")
 
-module.exports.run = async (bot, message, args) => {
-
-  let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
-  if(!wUser) return message.channel.send("**User not found.**");
-  let warnlevel = warns[wUser.id].warns;
-
-  message.channel.send(`<@${wUser.id}> has \`${warnlevel}\` warnings.`);
-
-}
-
-module.exports.help = {
-  name: "warnlevel"
-}
+module.exports = {
+    name: 'warnlevel',
+    description: 'Shows how many warns a user had.',
+    usage: '<user>',
+    args: true,
+    aliases: ['warnlvl'],
+	execute(bot, message, args){
+    let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
+    if(!wUser) return message.channel.send("**User not found.**");
+    let warnlevel = warns[wUser.id].warns;
+  
+    message.channel.send(`<@${wUser.id}> has \`${warnlevel}\` warnings.`);
+	},
+};
