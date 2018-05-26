a1e2f68d9db1768d5661f4be3d12f20f84cdbbca
tetragamingtg@gmail.com <tetragamingtg@gmail.com>
2018-05-26 09:25:54 -0700

    Help command done

--- commands/help.js
+++ commands/help.js
@@ -9,10 +9,10 @@ module.exports = {
         const { commands } = message.client;
 		const data = [];
 
-		if (!args.length) {
-			data.push('Here\'s a list of all my commands:');
+		if (!args.length) {/** 
+			data.push('Commands');
 			data.push(commands.map(command => command.name).join(', '));
-            data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
+            data.push(`\nYou can send \`ium help [command name]\` to get info on a specific command!`);*/
             
             const serverEmbed = new Discord.RichEmbed()
             .setColor('#ffffff')
@@ -41,17 +41,15 @@ module.exports = {
 
 			if (command.description) data.push(`**Description:** ${command.description}`);
 			if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
-			if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
+			if (command.usage) data.push(`**Usage:** \`ium ${command.name} ${command.usage}\``);
 
-			//data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
+			//data.push(`**Cooldown:** ${command.cooldown || 1} second(s)`);
 		}
 
-		message.author.send(data, { split: true })
+		message.channel.send(data, { split: true })
 			.then(() => {
-				if (message.channel.type !== 'dm') {
-					message.channel.send('I\'ve sent you a DM with all my commands!');
-				}
+                //message.channel.send("here are my commands..")
 			})
-			.catch(() => message.reply('It seems like I can\'t DM you!'));
+			.catch(() => message.reply('Something went wrong!'));
 	},
 };
