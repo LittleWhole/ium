bd4861067c75cb0fef496b45902b026e6c4511bb
tetragamingtg@gmail.com <tetragamingtg@gmail.com>
2018-05-25 19:48:15 -0700

    Reload command handler

--- commands/reload.js
+++ commands/reload.js
@@ -2,19 +2,23 @@ const Discord = require("discord.js")
 const fs = require("fs");
 const errors = require("../utils/errors.js")
 
-module.exports.run = async (bot, message, args) => {
-
-    if (!['275831434772742144',].includes(message.author.id)) return errors.noTetra(message, "reload");
+module.exports = {
+    name: 'reload',
+    description: 'Reloads the command you provide.',
+    usage: '<command>',
+    args: true,
+	async execute(bot, message, args){
+        if (!['275831434772742144',].includes(message.author.id)) return errors.noTetra(message, "reload");
     
-    try{
-        delete require.cache[require.resolve(`./${args[0]}.js`)];
-        let reloadE = new Discord.RichEmbed()
-        .setTitle("Reloading.. <a:loading:393852367751086090>")
-        .setDescription(`${args[0]}.js successfully reloaded.`)
-    
-        return message.channel.send(reloadE);
-    }catch(e){
-        return message.channel.send("**Command not found.**")
-    }
+        try{
+            delete require.cache[require.resolve(`./${args[0]}.js`)];
+            let reloadE = new Discord.RichEmbed()
+            .setTitle("Reloading.. <a:loading:393852367751086090>")
+            .setDescription(`${args[0]}.js successfully reloaded.`)
+        
+            return message.channel.send(reloadE);
+        }catch(e){
+            return message.channel.send("**Command not found.**")
+        }
+	},
 };
-   
