aa6845fafdd13a39f3d8c3d2ae1a9c814c167235
tetragamingtg@gmail.com <tetragamingtg@gmail.com>
2018-05-25 21:09:13 -0700

    Say Command Handler

--- commands/say.js
+++ commands/say.js
@@ -1,17 +1,13 @@
-module.exports.run = async (bot, message, args) => {
-    if(!args[0]) return message.channel.send("**Provide some text for me to say.** `ium say <text>`");
-    const sayMessage = args.join(" ");
-
-    //message.delete().catch(O_o=>{});
-    message.channel.send(sayMessage);
-
-}
-
-/** module.export.config = {
-    //name: ["say", "talk", "repeat"]
-}
-*/
-
-module.exports.help = {
-    name: "say"
-  }
+module.exports = {
+    name: 'say',
+    description: 'Says what you say.',
+    usage: '<text>',
+    args: true,
+    aliases: ['echo', 'repeat'],
+	async execute(bot, message, args){
+        const sayMessage = args.join(" ");
+    
+        //message.delete().catch(O_o=>{});
+        message.channel.send(sayMessage);
+	},
+};
