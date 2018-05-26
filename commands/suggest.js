16db36fb332406afa1934f8f968ac71759cff876
tetragamingtg@gmail.com <tetragamingtg@gmail.com>
2018-05-26 08:48:37 -0700

    Support Aliase in discord command

--- commands/suggest.js
+++ commands/suggest.js
@@ -6,17 +6,17 @@ var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database(dbFile);
 
 module.exports = {
-	name: 'suggest',
+    name: 'suggest',
     description: 'Suggest a command for ium.',
     usage: '<suggestion>',
     args: true,
     execute(bot, message, args){
-    db.serialize(function(){
-            if (!exists) {
-                    db.run('CREATE TABLE suggestions ( ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, Content varchar(2000), Author varchar(32) );');
-                    console.log('New table suggestions created!');
-            }
-    });
+        db.serialize(function(){
+                if (!exists) {
+                        db.run('CREATE TABLE suggestions ( ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, Content varchar(2000), Author varchar(32) );');
+                        console.log('New table suggestions created!');
+                }
+        });
 	db.run(`INSERT INTO suggestions (Content, Author) VALUES ('${args.join(" ")}', '${message.author.id}');`);
 	db.each(`SELECT * FROM suggestions WHERE Author='${message.author.id}' AND Content='${args.join(" ")}';`, function(err, row){
         let suggestEmbed = new Discord.RichEmbed()
