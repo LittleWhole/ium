d2512774bbbc1b5d249699f76916fb2604d20414
tetragamingtg@gmail.com <tetragamingtg@gmail.com>
2018-05-25 20:09:58 -0700

    Restart command handler

--- commands/restart.js
+++ commands/restart.js
@@ -1,10 +1,12 @@
 const errors = require("../utils/errors.js")
 
-module.exports.run = async (bot, message) => {
-
+module.exports = {
+    name: 'restart',
+    description: 'Restarts ium.',
+	async execute(bot, message, args){
     if (!['275831434772742144',].includes(message.author.id)) return errors.noTetra(message, "restart");
     message.channel.send(`**Restarted** in ${Math.floor(bot.ping)}ms`).then(() =>{
     process.exit(1);
   })
+	},
 };
-   
