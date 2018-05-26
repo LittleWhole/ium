37707bcc3eab680f3ff401eab950f218fc70885b
tetragamingtg@gmail.com <tetragamingtg@gmail.com>
2018-05-25 18:33:51 -0700

    Ping Command Handler

--- commands/ping.js
+++ commands/ping.js
@@ -3,15 +3,15 @@ const ms = require("ms");
 const bot = new Discord.Client({disableEveryone: true});
 bot.commands = new Discord.Collection();
 
-exports.run = async (bot, message, args) => {
-    const m = await message.channel.send("<a:cursor:404001393360502805> Pinging...");
-    const pingEmbed = new Discord.RichEmbed()
-    .setColor('#ffffff')
-    .addField("🏓 Pong!", `Latency \`${m.createdTimestamp - message.createdTimestamp}ms\`\n API Latency \`${Math.round(bot.ping)}ms\``)
-
-    m.edit(pingEmbed);
-}
-
-module.exports.help = {
-    name: "ping"
-  }
+module.exports = {
+    name: 'ping',
+    description: 'Sends you the bot\'s ping and API latency',
+	async execute(bot, message, args){
+        const m = await message.channel.send("<a:cursor:404001393360502805> Pinging...");
+        const pingEmbed = new Discord.RichEmbed()
+        .setColor('#ffffff')
+        .addField("🏓 Pong!", `Latency \`${m.createdTimestamp - message.createdTimestamp}ms\`\n API Latency \`${Math.round(bot.ping)}ms\``)
+    
+        m.edit(pingEmbed);
+	},
+};
