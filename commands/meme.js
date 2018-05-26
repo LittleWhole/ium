7ab4ddfd0a98d16c284fb9ba3e7459283ca29093
tetragamingtg@gmail.com <tetragamingtg@gmail.com>
2018-05-25 17:36:21 -0700

    Meme Command Handler

--- commands/meme.js
+++ commands/meme.js
@@ -2,12 +2,17 @@
 const Discord = require("discord.js");
 const meme = require('memejs');
 
-exports.run = async (bot, message, args, level) => {
-	let searchMessage = await message.channel.send('<a:typing:393848431413559296> Searching for memes in the database... ');
-  meme(function(data) {
-  const embed = new Discord.RichEmbed()
-  .setTitle(data.title[0])
-  .setColor("RANDOM")
-  .setImage(data.url[0])
-  searchMessage.edit({embed});
-})};
+module.exports = {
+    name: 'meme',
+    description: 'Sends 👏 an 👏 amazing 👏 meme.',
+	async execute(bot, message, args){
+    let searchMessage = await message.channel.send('<a:typing:393848431413559296> Searching for memes in the database... ');
+    meme(function(data) {
+    const embed = new Discord.RichEmbed()
+    .setTitle(data.title[0])
+    .setColor("RANDOM")
+    .setImage(data.url[0])
+    searchMessage.edit({embed});
+  })
+	},
+};
