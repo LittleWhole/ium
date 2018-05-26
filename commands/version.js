224a6fa35b33982586e8dc92695318ccfb16cd9d
tetragamingtg@gmail.com <tetragamingtg@gmail.com>
2018-05-26 08:59:22 -0700

    Version command handler

--- commands/version.js
+++ commands/version.js
@@ -2,17 +2,17 @@ const Discord = require("discord.js");
 const config = require("../botconfig.json");
 let version = config.version;
 
-
-exports.run = async (bot, message, args) => {
-    let verEmbed = new Discord.RichEmbed()
-    .setDescription("Version")
-    .setColor('#ffffff')
-    .addField("Current Version", config.version)
-    .addField("Made with", "discord.js v11.3 \nnpm v11.3.2 \nnode.js v8.10.0 and 💗");
-    
-    message.channel.send(verEmbed);
-}
-
-module.exports.help = {
-    name: "version"
-  }
+module.exports = {
+    name: 'version',
+    description: 'Shows ium\'s current version',
+    aliases: ['ver'],
+	execute(bot, message, args){
+        let verEmbed = new Discord.RichEmbed()
+        .setDescription("Version")
+        .setColor('#ffffff')
+        .addField("Current Version", config.version)
+        .addField("Made with", "discord.js v11.3 \nnpm v11.3.2 \nnode.js v8.10.0 and 💗");
+        
+        message.channel.send(verEmbed);
+	},
+};
