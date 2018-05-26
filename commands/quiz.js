eb86cf840ea18893941003edb6ce5b60442a3ce4
tetragamingtg@gmail.com <tetragamingtg@gmail.com>
2018-05-25 19:28:24 -0700

    Quiz Command Handler

--- commands/quiz.js
+++ commands/quiz.js
@@ -27,27 +27,27 @@ const options = {
   errors: ["time"],
 };
 
-module.exports.run = async (bot, message, args) => {
-  
-  const item = quiz[Math.floor(Math.random() * quiz.length)];
-  await message.channel.send(item.q);
-  try {
-    const collected = await message.channel.awaitMessages(answer => item.a.includes(answer.content.toLowerCase()), options);
-    const winnerMessage = collected.first();
-    return message.channel.send({embed: new Discord.RichEmbed()
-                                 .setAuthor(`Winner: ${winnerMessage.author.tag}`, winnerMessage.author.displayAvatarURL)
-                                 .setTitle(`Correct Answer: \`${winnerMessage.content}\``)
-                                 .setFooter(`Question: ${item.q}`)
-                                 .setColor(message.guild.me.displayHexColor)
-                                })
-  } catch (_) {
-    return message.channel.send({embed: new Discord.RichEmbed()
-                                 .setAuthor('No one got the answer in time!')
-                                 .setTitle(`Correct Answer(s): \`${item.a}\``)
-                                 .setFooter(`Question: ${item.q}`)
-                                })
-  }
-}
-module.exports.help = {
-name: "quiz"
-}
+module.exports = {
+    name: 'quiz',
+    description: 'Start a quiz, and answer questions.',
+	async execute(bot, message, args){
+    const item = quiz[Math.floor(Math.random() * quiz.length)];
+    await message.channel.send(item.q);
+    try {
+      const collected = await message.channel.awaitMessages(answer => item.a.includes(answer.content.toLowerCase()), options);
+      const winnerMessage = collected.first();
+      return message.channel.send({embed: new Discord.RichEmbed()
+                                   .setAuthor(`Winner: ${winnerMessage.author.tag}`, winnerMessage.author.displayAvatarURL)
+                                   .setTitle(`Correct Answer: \`${winnerMessage.content}\``)
+                                   .setFooter(`Question: ${item.q}`)
+                                   .setColor(message.guild.me.displayHexColor)
+                                  })
+    } catch (_) {
+      return message.channel.send({embed: new Discord.RichEmbed()
+                                   .setAuthor('No one got the answer in time!')
+                                   .setTitle(`Correct Answer(s): \`${item.a}\``)
+                                   .setFooter(`Question: ${item.q}`)
+                                  })
+    }
+	},
+};
