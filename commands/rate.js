b7eb17483f30c093a4e5e98e373d2c09d26ee7a3
tetragamingtg@gmail.com <tetragamingtg@gmail.com>
2018-05-25 19:39:47 -0700

    Rate command fix, and command handler

--- commands/rate.js
+++ commands/rate.js
@@ -1,25 +1,24 @@
 const Discord = require("discord.js")
 
-module.exports.run = async (bot, message, args) => {
+module.exports = {
+    name: 'rate',
+    description: 'Rates someone or something you provide from a scale of 1-10',
+    usage: '<someone/something>',
+    args: true,
+    aliases: ['ign'],
+	async execute(bot, message, args){
+    let ratings = ["0", "⭐ 1", "⭐⭐ 2", "⭐⭐⭐ 3", "⭐⭐⭐ 4", "⭐⭐⭐⭐⭐ 5", "⭐⭐⭐⭐⭐⭐ 6",  "⭐⭐⭐⭐⭐⭐ 7", "⭐⭐⭐⭐⭐⭐⭐⭐ 8", "⭐⭐⭐⭐⭐⭐⭐⭐⭐ 9", "⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐ 10"];
 
-   if(!args[0]) return message.channel.send("**Ask me to rate someone or something** `ium rate <someone/something>`");
-   let ratings = ["0", "⭐ - 1", "⭐⭐ - 2", "⭐⭐⭐ - 3", "⭐⭐⭐⭐- 4", "⭐⭐⭐⭐⭐ - 5", "⭐⭐⭐⭐⭐⭐ - 6",  "⭐⭐⭐⭐⭐⭐ - 7", "⭐⭐⭐⭐⭐⭐⭐⭐ - 8", "⭐⭐⭐⭐⭐⭐⭐⭐⭐ - 9", "⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐ - 10"];
-
-   let result = Math.floor((Math.random() * ratings.length));
-   let user = message.mentions.users.first();
-
-   let rateEmbed = new Discord.RichEmbed()
-
-   .setAuthor(message.author.username)
-   .setColor("#000000")
-   .addField("Something", args[0])
-   .addField("Rating", ratings[result]);
-
-   message.channel.send(rateEmbed)
-
-
-}
-
-module.exports.help = {
-    name: "rate"
-  }
+    let result = Math.floor((Math.random() * ratings.length));
+    let user = message.mentions.users.first();
+ 
+    let rateEmbed = new Discord.RichEmbed()
+ 
+    .setAuthor(message.author.username)
+    .setColor("#000000")
+    .addField("Rate", args[0])
+    .addField("Rating", ratings[result]);
+ 
+    message.channel.send(rateEmbed)
+	},
+};
