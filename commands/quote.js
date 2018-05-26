b469484b4106c39da2d5392878e3c7831832d11b
LittleWhole <wholelittle@gmail.com>
2018-05-26 16:12:51 -0400

    Update quote.js

--- commands/quote.js
+++ commands/quote.js
@@ -13,7 +13,7 @@ module.exports = {
         var quote = new Discord.RichEmbed()
         .setAuthor(`${author.tag} said:`, author.avatarURL)
         .setDescription(`${msg.content}`)
-        .setFooter(`Quoted by ${message.author.tag}`)
+        .setFooter(`Quoted by ${message.author.tag}`, message.author.avatarURL)
         .setColor(0x36393e);
       } catch(e) {
         var quote = new Discord.RichEmbed()
@@ -23,6 +23,23 @@ module.exports = {
         .setColor(0x36393e);
       } finally {
          message.channel.send({embed: quote});
+	 if (embeds) {
+                let messageEmbed = new Discord.RichEmbed()
+                if (embeds.title) messageEmbed = messageEmbed.setTitle(embeds.title);
+                if (embeds.description) messageEmbed = messageEmbed.setDescription(embeds.description);
+                if (embeds.thumbnail) messageEmbed = messageEmbed.setThumbnail(embeds.description);
+                if (embeds.footer) messageEmbed = messageEmbed.setFooter(embeds.footer.text, embeds.footer.iconURL);
+                if (embeds.image) messageEmbed = messageEmbed.setImage(embeds.image.url);
+                if (embeds.hexColor) messageEmbed = messageEmbed.setColor(embeds.hexColor);
+                if (embeds.timestamp) messageEmbed = messageEmbed.setTimestamp();
+                if (embeds.url) messageEmbed = messageEmbed.setURL(embeds.url);
+                if (embeds.fields) {
+                  for (let i = 0; i < embeds.fields.length; i++) {
+                    messageEmbed = messageEmbed.addField(embeds.fields[i].name, embeds.fields[i].value, embeds.fields[i].inline);                    
+                  }
+                }
+                message.channel.send({embed: messageEmbed})
+       }
       }
     },
 };
