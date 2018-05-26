ed2fc5ab6f7bcd6b7885d10c8067065e40a9e3bb
tetragamingtg@gmail.com <tetragamingtg@gmail.com>
2018-05-26 08:53:44 -0700

    Trello command handler

--- commands/trello.js
+++ commands/trello.js
@@ -1,13 +1,16 @@
 const Discord = require("discord.js");
 
-exports.run = async (bot, message, args) => {
-    let devEmbed = new Discord.RichEmbed()
-    .setColor('#ffffff ')
-    .addField("ium's trello", "https://trello.com/b/3kvrAbIV/ium")
-
-    message.channel.send(devEmbed);
-}
-
-module.exports.help = {
-    name: "trello"
-  }
+  const Discord = require("discord.js");
+  const meme = require('memejs');
+  
+  module.exports = {
+      name: 'trello',
+      description: 'Provides a link to the trello (to do list) for ium.',
+      execute(bot, message, args){
+        let devEmbed = new Discord.RichEmbed()
+        .setColor('#ffffff ')
+        .addField("ium's trello", "https://trello.com/b/3kvrAbIV/ium")
+    
+        message.channel.send(devEmbed);
+      },
+  };
