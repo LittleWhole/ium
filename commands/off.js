b1a4b2aab2980c3448c2c48bad0bb9d048e60548
tetragamingtg@gmail.com <tetragamingtg@gmail.com>
2018-05-25 18:02:41 -0700

    off command handler

--- commands/off.js
+++ commands/off.js
@@ -1,7 +1,11 @@
 const errors = require("../utils/errors.js")
 
-module.exports.run = async (bot, message, args) => {
+module.exports = {
+    name: 'off',
+    description: 'Shuts down ium.',
+	async execute(bot, message, args){
     if (!['275831434772742144',].includes(message.author.id)) return errors.noTetra(message, "off");
     await message.reply('**Shutting** down...');
     process.exit(0);
-  };
+	},
+};
