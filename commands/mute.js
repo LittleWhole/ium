da1b9d7c027a8ae628cd85a8bf557986c9a55f81
tetragamingtg@gmail.com <tetragamingtg@gmail.com>
2018-05-25 17:51:53 -0700

    Mute Command Handler

--- commands/mute.js
+++ commands/mute.js
@@ -2,50 +2,49 @@ const Discord = require("discord.js");
 const errors = require("../utils/errors.js")
 const ms = require("ms");
 
-exports.run = async (bot, message, args) => {
-
-  if(tomute.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "mute");
-      let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-      if(!tomute) return message.channel.send("**User not found.** `ium mute <user> <time (optional)>`");
-      let muterole = message.guild.roles.find(`name`, "muted");
-      //start of create role
-      if(!muterole){
-        try{
-          muterole = await message.guild.createRole({
-            name: "muted",
-            color: "#000000",
-            permissions:[]
-          })
-          message.guild.channels.forEach(async (channel, id) => {
-            await channel.overwritePermissions(muterole, {
-              SEND_MESSAGES: false,
-              ADD_REACTIONS: false
-            });
+module.exports = {
+    name: 'magik',
+    description: 'Mocks someone or something.',
+    usage: '<user>',
+    args: true,
+	async execute(bot, message, args){
+    if(tomute.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "mute");
+    let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!tomute) return message.channel.send("**User not found.** `ium mute <user>`");
+    let muterole = message.guild.roles.find(`name`, "muted");
+    //start of create role
+    if(!muterole){
+      try{
+        muterole = await message.guild.createRole({
+          name: "muted",
+          color: "#000000",
+          permissions:[]
+        })
+        message.guild.channels.forEach(async (channel, id) => {
+          await channel.overwritePermissions(muterole, {
+            SEND_MESSAGES: false,
+            ADD_REACTIONS: false
           });
-        }catch(e){
-          console.log(e.stack);
-        }
+        });
+      }catch(e){
+        console.log(e.stack);
       }
-      //end of create role
-      let mutetime = args[1];
-      if(!mutetime){
-        //message.channel.send("**Specify a time for the user to be muted.**");
-
-        await(tomute.addRole(muterole.id));
-        message.send.channel('<@${tomute.id}> has been muted. (Note: you can set a time for the user to be muted: `ium mute <user> <time>`)');
-      }
+    }
+    //end of create role
+    let mutetime = args[1];
+    if(!mutetime){
+      //message.channel.send("**Specify a time for the user to be muted.**");
 
       await(tomute.addRole(muterole.id));
-      message.send.channel(`<@${tomute.id}> has been muted for ${ms(ms(mutetime))}`);
-
-      setTimeout(function(){
-        tomute.removeRole(muterole.id);
-        message.channel.send(`<@${tomute.id}> has been unmuted!`);
-      }, ms(mutetime));
-
-
+      message.send.channel('<@${tomute.id}> has been muted.');
     }
 
-    module.exports.help = {
-      name: "mute"
-    }
+    await(tomute.addRole(muterole.id));
+    message.send.channel(`<@${tomute.id}> has been muted for ${ms(ms(mutetime))}`);
+
+    setTimeout(function(){
+      tomute.removeRole(muterole.id);
+      message.channel.send(`<@${tomute.id}> has been unmuted!`);
+    }, ms(mutetime));
+	},
+};
