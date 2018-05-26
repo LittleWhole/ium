3d255ed2b8c36248049062723c619e7d25140576
tetragamingtg@gmail.com <tetragamingtg@gmail.com>
2018-05-26 09:09:38 -0700

    ym aliase

--- commands/yomama.js
+++ commands/yomama.js
@@ -3,7 +3,7 @@ const yoMamma = require('yo-mamma').default;
 module.exports = {
     name: 'yomama',
     description: 'Sends a yomama joke.',
-    aliases: ['yo-mamma'],
+    aliases: ['yo-mamma', 'ym'],
 	execute(bot, message, args){
         let joke = yoMamma();
         message.channel.send(joke)
