dc5289eaedc8058400dd03bb1786fe6541e29b6b
tetragamingtg@gmail.com <tetragamingtg@gmail.com>
2018-05-25 17:39:29 -0700

    Mock COmmand Handler

--- commands/mock.js
+++ commands/mock.js
@@ -1,19 +1,17 @@
 const Discord = require("discord.js");
 const randomizeCase = word => word.split('').map(c => Math.random() > 0.5 ? c.toUpperCase() : c.toLowerCase()).join('');
 
-exports.run = async (bot, message, args) => {
-    if (args.length < 1) return message.channel.send("**I need some text to mock.** `ium mock Zucc`")
-
-    let mockEmbed = new Discord.RichEmbed()
-    .setColor(`RANDOM`)
-    .setDescription(args.map(randomizeCase))
-    .setImage("https://cdn.discordapp.com/attachments/424889733043191810/425242569325150208/mock.jpg")
-
-    message.channel.send(mockEmbed);
-
-}
-
-
-module.exports.help = {
-    name: "mock"
-  }
+module.exports = {
+    name: 'magik',
+    description: 'Mocks someone or something.',
+    usage: '<someone/something>',
+    args: true,
+	async execute(bot, message, args){
+        let mockEmbed = new Discord.RichEmbed()
+        .setColor(`RANDOM`)
+        .setDescription(args.map(randomizeCase))
+        .setImage("https://cdn.discordapp.com/attachments/424889733043191810/425242569325150208/mock.jpg")
+    
+        message.channel.send(mockEmbed);
+	},
+};
