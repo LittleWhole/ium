28739c6183028f27922bf3209bdbca427d0f3cb3
tetragamingtg@gmail.com <tetragamingtg@gmail.com>
2018-05-26 09:08:27 -0700

    wur command handler

--- commands/wur.js
+++ commands/wur.js
@@ -2,15 +2,19 @@ const Discord = require('discord.js');
 const superagent = require("superagent");
 const send = require("quick.hook");
 
-exports.run = async(bot, message, args) => {
-
-    const { body } = await superagent 
-    .get('http://www.rrrather.com/botapi'); 
-    const rEmbed = new Discord.RichEmbed() 
-    .setTitle(`${body.title}`)
-    .setURL(body.link)
-    .setDescription(`${body.choicea} or ${body.choiceb}?`)
-    .setFooter('Powered by rrather');
-
-    message.channel.send(rEmbed);
-}
+module.exports = {
+    name: 'wur',
+    description: 'Sends you a would you rather.',
+    aliases: ['wouldyourather'],
+	async execute(bot, message, args){
+        const { body } = await superagent 
+        .get('http://www.rrrather.com/botapi'); 
+        const rEmbed = new Discord.RichEmbed() 
+        .setTitle(`${body.title}`)
+        .setURL(body.link)
+        .setDescription(`${body.choicea} or ${body.choiceb}?`)
+        .setFooter('Powered by rrather');
+    
+        message.channel.send(rEmbed);
+	},
+};
