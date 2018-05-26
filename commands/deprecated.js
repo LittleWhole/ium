ce465f0d5f2a38e7085bec1f50b2a0039a55fa35
tetragamingtg@gmail.com <tetragamingtg@gmail.com>
2018-05-26 09:04:09 -0700

    wave command deprecated

--- commands/deprecated.js
+++ commands/deprecated.js
@@ -5,12 +5,12 @@ const errors = require("../utils/errors.js")
   module.exports = {
       name: 'depricated',
       description: 'Shows depricated commands.',
-      aliases: ['divide', 'subtract', 'add', 'multiply', 'minus', 'times', 'plus'],
+      aliases: ['divide', 'subtract', 'add', 'multiply', 'minus', 'times', 'plus', 'wave', 'scale', 'day', 'date', 'report'],
       execute(bot, message, args){
         let deprecatedEmbed = new Discord.RichEmbed()
         .setColor("#36393e")
         .setDescription(`This command is now deprecated <a:cursor:404001393360502805>`)
-        .addField(`Depricated Commands`, "`add` -> `calculate`\n`divide` -> `calculate`\n`minus` -> `calculate`\n`times` -> `calculate`\n`exponent` -> `calculate`\n`day`\n`report`\n`scale`")
+        .addField(`Depricated Commands`, "`add` -> `calculate`\n`divide` -> `calculate`\n`minus` -> `calculate`\n`times` -> `calculate`\n`exponent` -> `calculate`\n`day`\n`report`\n`scale`\n`wave`")
         .setTimestamp();
         message.channel.send(deprecatedEmbed);
       },
