c3f985bb8b0ab2993dc34079ece583b0bf3c52ee
tetragamingtg@gmail.com <tetragamingtg@gmail.com>
2018-05-25 18:09:17 -0700

    Patreon Command Handler

--- commands/patreon.js
+++ commands/patreon.js
@@ -1,16 +1,14 @@
 const Discord = require("discord.js");
 let inline = true
 
-exports.run = async (bot, message, args) => {
-    let botAvatar = bot.user.displayAvatarURL;
-    let botEmbed = new Discord.RichEmbed()
-    .setColor('#000000')
-    .setDescription("ium's patreon: https://www.patreon.com/ium")
-
-    message.channel.send(botEmbed);
-}
-
-
-module.exports.help = {
-    name: "patreon"
-}
+module.exports = {
+    name: 'off',
+    description: '',
+	async execute(bot, message, args){
+        let botEmbed = new Discord.RichEmbed()
+        .setColor('#000000')
+        .setDescription("ium's patreon: https://www.patreon.com/ium")
+    
+        message.channel.send(botEmbed);
+	},
+};
