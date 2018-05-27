74e1bc72662bc3a7d9b14b656b580657c68e448c
tetragamingtg@gmail.com <tetragamingtg@gmail.com>
2018-05-27 10:27:52 -0700

    birb aliase removed from cat command

--- commands/cat.js
+++ commands/cat.js
@@ -5,7 +5,7 @@ const request = require("request");
 module.exports = {
 	name: 'cat',
     description: 'Displays a random picture of a cat.',
-    aliases: ['birb', 'meow'],
+    aliases: ['meow'],
     async execute (bot, message, args) {
         request('http://edgecats.net/random', function (error, response, body) {
             if (!error && response.statusCode == 200) {
